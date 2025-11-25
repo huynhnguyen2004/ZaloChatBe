356425323731c1b3b36d3a7d517ca8a495301c9d
HuynhPC <huynhdang2332004@gmail.com>
2025-11-25 18:32:22 +0700

    khởi tạo dự án chat realtime

---
+++ src/main/java/com/huynh/ZaloCloneBe/ZaloCloneBeApplication.java
@@ -0,0 +1,13 @@
+package com.huynh.ZaloCloneBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaloCloneBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaloCloneBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huynh/ZaloCloneBe/ZaloCloneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huynh.ZaloCloneBe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaloCloneBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
