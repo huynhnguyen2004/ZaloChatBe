4b000e7fc2923a78894ff49ac7e25b46e13c470c
HuynhPC <huynhdang2332004@gmail.com>
2025-12-17 21:26:50 +0700

    api huy ket ban

--- src/main/java/com/huynh/ZaloCloneBe/repository/FriendRepository.java
+++ src/main/java/com/huynh/ZaloCloneBe/repository/FriendRepository.java
@@ -1,7 +1,9 @@
 package com.huynh.ZaloCloneBe.repository;
 
 import com.huynh.ZaloCloneBe.entity.Friend;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,6 +16,14 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
     @Query("SELECT f FROM Friend f WHERE f.user1.id = :userId OR f.user2.id = :userId")
     List<Friend> findFriends(@Param("userId") Long userId);
     boolean existsByUser1IdAndUser2Id(Long user1, Long user2);
+    @Transactional
+    @Modifying
+    @Query("""
+    Delete from Friend f
+    where (f.user1.id=:a and f.user2.id=:b)
+    or (f.user1.id=:b and f.user2.id=:a)
+""")
+    void unFriend(@Param("a")Long a,@Param("b") Long b);
 
 
 
--- src/main/java/com/huynh/ZaloCloneBe/service/FriendService.java
+++ src/main/java/com/huynh/ZaloCloneBe/service/FriendService.java
@@ -3,6 +3,8 @@ package com.huynh.ZaloCloneBe.service;
 import com.huynh.ZaloCloneBe.dto.response.FriendResponse;
 import com.huynh.ZaloCloneBe.entity.Friend;
 import com.huynh.ZaloCloneBe.entity.User;
+import com.huynh.ZaloCloneBe.exception.AppException;
+import com.huynh.ZaloCloneBe.exception.ErrorCode;
 import com.huynh.ZaloCloneBe.repository.FriendRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -36,5 +38,14 @@ public class FriendService {
                             .build();
                 })
                 .toList();
+    }
+   public void unFriend(Long user1Id,Long user2Id){
+        boolean isFriend= friendRepository.existsByUser1IdAndUser2Id(user1Id, user2Id);
+        if(!isFriend){
+            throw new AppException(ErrorCode.FRIEND_notfound);
+        }else{
+            friendRepository.unFriend(user1Id, user2Id);
+        }
+
     }
 }
